fef507c7fb9d3651a980929980f1da97b2047d00
Fayaz <fayazsalim@gmail.com>
2022-09-24 15:48:42 +0530

    Modifying APIs

--- src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
+++ src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
@@ -1,32 +1,12 @@
 package com.example.mdbspringboot;
 
-import com.example.mdbspringboot.model.User;
-import com.example.mdbspringboot.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication()
-public class MdbSpringBootApplication implements CommandLineRunner {
-   private final UserRepository userRepository;
-
-   public MdbSpringBootApplication(UserRepository userRepository){
-       this.userRepository = userRepository;
-   }
-
+public class MdbSpringBootApplication {
    public static void main(String[] args) {
        SpringApplication.run(MdbSpringBootApplication.class, args);
-   }
-
-   @Override
-    public void run(String...args) throws Exception {
-       userRepository.deleteAll();
-       userRepository.save(new User("Alice", "Vanderbilt x"));
-       userRepository.save(new User("Morty", "Smith"));
-       userRepository.save(new User("Summer", "Smith"));
-       for (User user : userRepository.findAll()){
-           System.out.println(user);
-       }
    }
 }
--- src/main/java/com/example/mdbspringboot/controller/UserController.java
+++ src/main/java/com/example/mdbspringboot/controller/UserController.java
@@ -2,8 +2,8 @@ package com.example.mdbspringboot.controller;
 
 import com.example.mdbspringboot.model.User;
 import com.example.mdbspringboot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +19,17 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/add")
+    public List<User> addUser(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName){
+        userRepository.save(new User(firstName, lastName));
+        return getUsers();
+    }
+
+    @GetMapping("/delete")
+    public List<User> deleteUser (@RequestParam(value = "firstName") String firstName){
+        userRepository.deleteUserByFirstName(firstName);
+        return getUsers();
     }
 }
